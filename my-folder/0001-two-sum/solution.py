6773c1756f60412cefc7c4ada20caccadd51e101
jsxtw <justintwx7@gmail.com>
2025-10-18 03:18:31 +0000

    [LeetCode Sync] Runtime - 8 ms (32.77%), Memory - 19.5 MB (99.91%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,7 +1,18 @@
-class Solution(object):
-  def twoSum(self, nums, target):
-    for i in range(len(nums)):
-        for j in range(i+1, len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i, j]
-    return []
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_2 = list(enumerate(nums))
+        num_2.sort(key=lambda x:x[1])
+
+        left, right = 0, len(num_2) - 1
+
+        while left < right: 
+            current_sum = num_2[left][1] + num_2[right][1] 
+            if current_sum == target: 
+                return [num_2[left][0], num_2[right][0]]
+            else:
+                if current_sum < target:
+                    left += 1
+                elif current_sum > target:
+                    right -= 1
+        return
+
