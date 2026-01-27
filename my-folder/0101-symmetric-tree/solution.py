6b84f049d19c20855c5a79945cc055220ccacc38
jsxtw <justintwx7@gmail.com>
2026-01-27 18:01:07 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.6 MB (17.86%)

---
+++ my-folder/0101-symmetric-tree/solution.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def isSame(left, right):
+            if left is None and right is None:
+                return True
+            if left is None or right is None:
+                return False
+
+            return (
+                left.val == right.val and
+                isSame(left.left, right.right) and
+                isSame(left.right, right.left)
+            )
+
+        return isSame(root.left, root.right)
+
+        
+        
